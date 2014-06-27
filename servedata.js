1c0d53f9edad426c58dca860b8792ab17c887194
naomi <naomi@l0l.org.uk>
2014-06-27 21:21:16 +0100

    initial

---
+++ servedata.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var cql = require('node-cassandra-cql');
+var app = express();
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+};
+
+app.use(allowCrossDomain);
+
+var client = new cql.Client({
+    hosts: ['localhost'],
+    keyspace: 'aquaponics'
+});
+
+app.get("/todmorden", function (request, response) {
+		var range = getTimeRange(request);
+		console.log(range);
+    var cql = "select * from todmorden where reading_time >= '"+range.from+"' and reading_time <= '"+range.to+"' ALLOW FILTERING";
+    client.execute(cql, function(err, result){
+        if (err) {
+            response.send(err);
+        } else {
+            response.send(result);
+        }
+    });
+});
+
+app.get("/todmorden/:arg0", function (request, response) {
+		var range = getTimeRange(request);
+    var cql = "select * from todmorden where sensor_name = ? and reading_time >= '"+range.from+"' and reading_time <= '"+range.to+"' ALLOW FILTERING";
+    client.execute(cql, [request.params.arg0], function(err, result){
+        if (err) {
+            response.send(err);
+        } else {
+            response.send(result);
+        }
+    });
+});
+
+function getTimeRange(request) {
+	var url = require('url');
+	var url_parts = url.parse(request.url, true);
+	var from_array = url_parts.query.from.split('/');	
+	var to_array = url_parts.query.to.split('/');
+	var from;
+	var to;
+	if (from_array.length !== 3) {
+		from = "2014-06-26";
+	} else {
+		from = from_array[2]+'-'+from_array[1]+'-'+from_array[0];
+	}	
+	if (to_array.length !== 3) {
+		var midnight = new Date();
+		midnight.setDate(midnight.getDate() + 1);
+		to = midnight.getFullYear()+'-'+(midnight.getMonth() + 1)+'-'+midnight.getDate();
+	} else {
+		to = to_array[2]+'-'+to_array[1]+'-'+to_array[0];
+	}
+	return {
+		'from': from,
+		'to': to,
+  };
+};
+
+app.listen(8001);
+console.log("Server running at http://127.0.0.1:8001/");
