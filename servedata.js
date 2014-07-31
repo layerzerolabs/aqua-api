958d0a6c54d6dfda8ccc2ad409f3671a3efe9f81
layerzerolabs <info@l0l/org.uk>
2014-07-31 10:50:09 +0100

    Swagger integration

--- servedata.js
+++ servedata.js
@@ -1,6 +1,35 @@
-var express = require('express');
-var cql = require('node-cassandra-cql');
+var express = require('express')
+ , url = require("url")
+ , bodyParser = require("body-parser")
+ , swagger = require("swagger-node-express")
+ , cql = require('node-cassandra-cql');
+
 var app = express();
+
+app.use(bodyParser.json());
+
+// Couple the application to the Swagger module.
+swagger.setAppHandler(app);
+
+swagger.addModels({
+	"Reading":{
+      		"id":"Reading",
+      		"required": ["sensor_name", "reading_time", "reading_value"],
+	        "properties": {
+			"sensor_name": {
+		  		"type": "string",
+		  		"description": "Source of the message (e.g. 'Water Temperature', or 'system' if it is a system message)"
+			}, "reading_time": {
+				"type": "dateTime",
+				"description": "Date and time the message was created"
+			}, "reading_value": {
+				"type": "string",
+				"description": "Content of the message (e.g. '22.6' or 'Reset triggered')"
+			}
+		}
+	},	
+});
+
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Methods', 'GET');
@@ -10,27 +39,54 @@ var allowCrossDomain = function(req, res, next) {
 
 app.use(allowCrossDomain);
 
+var basePath = 'saltmarsh.webarch.net:8003';
+
 var client = new cql.Client({
     hosts: ['localhost'],
     keyspace: 'aquaponics'
 });
 
-app.get("/todmorden", function (request, response) {
-    var range = getTimeRange(request);
-    var cql_numeric = "select sensor_name, reading_time, reading_value from todmorden_numeric where reading_time >= '"+range.from+"' and reading_time <= '"+range.to+"' ALLOW FILTERING";
-    var cql_text = "select sensor_name, reading_time, reading_value from todmorden_text where reading_time >= '"+range.from+"' and reading_time <= '"+range.to+"' ALLOW FILTERING";
-    client.execute(cql_numeric, [request.params.arg0], function(numeric_err, numeric_result) {
-    	client.execute(cql_text, [request.params.arg0], function(text_err, text_result){
-	    if (numeric_err || text_err) {
-                response.send(numeric_err + text_err);        
-            } else {
-                response.send(numeric_result.rows.concat(text_result.rows));
-            }
-	});
-    });
-});
+var getAll = {
+	'spec': {
+		    "description" : "Get all sensor data",
+		    "path" : "/todmorden/all",
+		    "notes" : "Returns all sensor data",
+		    "summary" : "Get all sensor data",
+                    "method": "GET",
+	/*	    "parameters" : [
+			swagger.pathParam("from", "Date/time from", "timestamp"),
+			swagger.pathParam("to", "Date/time to", "timestamp"),
+                    ],
+        */
+		    "type" : "Reading",
+		    "errorResponses" : [
+			swagger.errors.invalid('from'), 
+			swagger.errors.invalid('to'),
+		    ],
+         
+		    "nickname" : "getAll"
+	},
+        action:  function (request, response) {
+		    var cql_numeric = "select sensor_name, reading_time, reading_value from todmorden_numeric where sensor_name = 'Light' order by reading_time desc limit 10";
+		    var cql_text = "select sensor_name, reading_time, reading_value from todmorden_text where sensor_name = 'Light' order by reading_time desc limit 10"; 
+		    // where reading_time >= '"+range.from+"' and reading_time <= '"+range.to+"' ALLOW FILTERING";
+		    //var range = getTimeRange(request);
+		    //if (!range) {
+				
+		    //}
+		    client.execute(cql_numeric, [request.params.arg0], function(numeric_err, numeric_result) {
+			client.execute(cql_text, [request.params.arg0], function(text_err, text_result){
+			    if (numeric_err || text_err) {
+				throw swagger.errors.notFound('readings');        
+			    } else {
+				response.send(numeric_result.rows.concat(text_result.rows));
+			    }
+			});
+		    });
+	}	
+};
 
-app.get("/todmorden/:arg0", function (request, response) {
+/*app.get("/todmorden/:arg0", function (request, response) {
     var range = getTimeRange(request);
     var cql_numeric = "select * from todmorden_numeric where sensor_name = ? and reading_time >= '"+range.from+"' and reading_time <= '"+range.to+"' ALLOW FILTERING";
     var cql_text = "select * from todmorden_text where sensor_name = ? and reading_time >= '"+range.from+"' and reading_time <= '"+range.to+"' ALLOW FILTERING";
@@ -43,11 +99,15 @@ app.get("/todmorden/:arg0", function (request, response) {
             }
 	});
     });
-});
+}); */
 
 function getTimeRange(request) {
 	var url = require('url');
 	var url_parts = url.parse(request.url, true);
+	if (typeof url_parts.query.from === 'undefined' 
+           || typeof url_parts.query.to === 'undefined') {
+		return false;	
+	}
 	var from_array = url_parts.query.from.split('/');	
 	var to_array = url_parts.query.to.split('/');
 	var from;
@@ -75,6 +135,8 @@ function nextDayString(date_obj) {
 	date_obj.setDate(date_obj.getDate() + 1);
 	return date_obj.getFullYear()+'-'+(date_obj.getMonth() + 1)+'-'+date_obj.getDate();
 }
-
+swagger.addGet(getAll);
+swagger.configure("http://saltmarsh.webarch.net:8003", "0.1");
+app.use(express.static(__dirname + '/node_modules/swagger-node-express/swagger-ui/'));
 app.listen(8003);
 console.log("Aqua API Server running on 8003");
