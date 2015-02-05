276568699847018ae5756efa8a8873921187dc02
layerzerolabs <info@l0l.org.uk>
2015-02-05 19:13:57 +0000

    swagger reads url now so no need to serve client settings

--- client-settings.js
+++ client-settings.js
@@ -1,4 +1,3 @@
-//These settings are used in servedata.js and also in the swagger ui.
 var settings = {
    baseUrl: 'http://localhost', //change to http://incredibleaquagdn.no-ip.info in production
    port: 8003
